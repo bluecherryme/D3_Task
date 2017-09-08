1d6bdbc4ffe9e09b51add38922b254639c3f79d1
Johanna Staedel <bluecherryme@gmail.com>
2017-09-08 15:15:46 +0200

    first chart completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,62 +1,51 @@
 $(document).ready(function(){
     
-    function timeConverter(timestamp){
-        var a = new Date(timestamp * 1000);
-        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-        var year = a.getFullYear();
-        var month = months[a.getMonth()];
-        var date = a.getDate();
-        var hour = a.getHours();
-        var min = a.getMinutes().toString();
-        min.length === 1 ? min ='0'+ min : null;
-        var sec = a.getSeconds().toString();
-        sec.length === 1 ? sec = '0' + sec : null;
-        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-        return time;
-    };
+    dataG = data.granular;
+    dataG.forEach(d => d.timestamp = new Date(d.timestamp*1000))
+    var width = 800;
+    var height = 300;
+    var margin = {top: 20, bottom: 20, left: 20, right: 20};
 
-    console.log(timeConverter(data.granular[2].timestamp));
+    //scales
+    var xExtent = d3.extent(dataG, d => d.timestamp); //finds min and max value
+    var xScale = d3.scaleTime()
+        .domain(xExtent)
+        .range([margin.left, width - margin.right]);
+    var yMax = d3.max(dataG, d => d.page_posts);
+    var yScale = d3.scaleLinear()
+        .domain([0, yMax])
+        .range([height - margin.bottom, margin.top]);
+    var heightScale = d3.scaleLinear()
+        .domain([0, yMax])
+        .range([0, height - margin.top - margin.bottom]);
 
-    var dataP = [1000,2500,1750,2000,1200,600,200];
-
+    //create the rectangles
     var svg = d3.select('svg');
 
     svg.selectAll('rect')
-        .data(dataP).enter()
-        .append('rect')
-        .attr('x', (d,i)=>i * 50)
-        .attr('y', d=> d)
-        .attr('width', 50)
-        .attr('height', d=> d)
+        .data(dataG)
+        .enter().append('rect')
+        .attr('width', 10)
+        .attr('height', d => heightScale(d.page_posts))
+        .attr('x', d => xScale(d.timestamp))
+        .attr('y', d => yScale(d.page_posts))
         .attr('fill', 'blue')
-        .attr('stroke', 'red');
+        .attr('stroke', 'white');
 
-        console.log(svg);
-
-
-    var yScale = d3.scaleLinear()
-        .domain([0,3000])  //input
-        .range([300,0]);  //output
-        console.log(yScale);
-        // var min = d3.min(data, d => [city]);
-        // var max = d3.max(data, d => d[city]);
-
-        // var extent = d3.extent(data, d => d[city]);
-
-        // var yScale = d3.scaleLinear()
-        //     .domain(extent)
-        //     .range([height,0]);
-
+    // create x and y axis
+    var xAxis = d3.axisBottom()
+        .scale(xScale)
+        .tickFormat(d3.timeFormat('%b $Y'));
     var yAxis = d3.axisLeft()
         .scale(yScale);
 
-    d3.select('svg')
-        .append('g')
-        .attr('transform', 'translate(40,20)')
+    svg.append('g')
+        .attr('transform', 'translate(' + [0, height - margin.bottom] + ')')
+        .call(xAxis);
+
+    svg.append('g')
+        .attr('transform', 'translate(' + [margin.left, 0] + ')')
         .call(yAxis);
-
-
-
 
 
 
